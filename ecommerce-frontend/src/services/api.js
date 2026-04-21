c218472e9290297997664663bcb68e323bc9cc31
Hricky10 <hrickysoftech@gmail.com>
2026-04-21 15:21:12 +0530

    Refactor API service to use axios instance
    Refactored API service to use axios instance for requests.

--- ecommerce-frontend/src/services/api.js
+++ ecommerce-frontend/src/services/api.js
@@ -1,11 +1,49 @@
 import axios from "axios";
 
+// Base URL of your backend
 const BASE_URL = "http://localhost:8080";
 
-export const getProducts = () => axios.get(BASE_URL + "/products");
+// Create axios instance (better practice)
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export const addProduct = (data) =>
-  axios.post(BASE_URL + "/products", data);
 
-export const login = (data) =>
-  axios.post(BASE_URL + "/users/login", data);
+
+
+// Get all products
+export const getProducts = () => {
+  return api.get("/products");
+};
+
+// Add new product
+export const addProduct = (product) => {
+  return api.post("/products", product);
+};
+
+
+
+
+// Register user
+export const registerUser = (user) => {
+  return api.post("/users/register", user);
+};
+
+// Login user
+export const login = (user) => {
+  return api.post("/users/login", user);
+};
+
+
+
+export const deleteProduct = (id) => {
+  return api.delete(`/products/${id}`);
+};
+
+// Example: update product
+export const updateProduct = (id, product) => {
+  return api.put(`/products/${id}`, product);
+};
