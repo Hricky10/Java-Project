346624482efb9ec1c4e949f1a5f56466c286b821
Hricky10 <hrickysoftech@gmail.com>
2026-04-21 15:05:43 +0530

    Initialize App component with Login and Products

---
+++ ecommerce-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Products from "./pages/Products";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <Login />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
